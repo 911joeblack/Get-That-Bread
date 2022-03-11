a224cbd0aea70c43a25b49f3d9063f2da8810c7f
isaacolvera <isaac_olvera712@csu.fullerton.edu>
2022-03-10 18:39:45 -0800

    Reworked all files and updated play page

---
+++ js_scripts/Login_page_script.js
@@ -0,0 +1,23 @@
+
+function myfunc()
+{
+  if(document.getElementById("Lname").value.length == 0
+    ||document.getElementById("fname").value.length == 0
+    ||document.getElementById("email").value.length == 0)
+      alert("Please fill in all of the blanks")
+
+  var name = document.getElementById("fname").value;
+  var list_of_name = ["Zhihui", "Alex", "Isaac", "Shivam", "Joel"]
+
+  if ((name != list_of_name[0]) && (name != list_of_name[1]) && (name != list_of_name[2]))
+    document.getElementById("demo").innerHTML = "Welcome " + name;
+
+  else
+    document.getElementById("demo").innerHTML = "Welcome back " + name;
+}
+
+var dt = new Date();
+document.getElementById("datetime").innerHTML = (("0" + dt.getDate()).slice(-2)) + ". " +
+  (("0" + (dt.getMonth() + 1)).slice(-2)) + ". " + (dt.getFullYear()) + " " +
+  (("0"+ dt.getHours()).slice(-2)) + ": "+
+  (("0"+ dt.getMinutes()).slice(-2));
---
+++ js_scripts/Play_page_script.js
@@ -0,0 +1,67 @@
+
+var score = 0;
+var clickingPower = 1;
+
+var cursorCost = 15;
+var totalCursors = 0;
+
+var grandpaCost = 100;
+var totalGrandpas = 0;
+
+var bakeryCost = 1000;
+var totalBakeries = 0;
+
+function buyCursor()
+{
+  if (score >= cursorCost)
+  {
+    score = score - cursorCost;
+    totalCursors = totalCursors + 1;
+    cursorCost = Math.round(cursorCost * 1.15);
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("cursor_cost").innerHTML = cursorCost;
+    document.getElementById("total_cursors").innerHTML = totalCursors;
+  }
+}
+
+function buyGrandpa()
+{
+  if (score >= grandpaCost)
+  {
+    score = score - grandpaCost;
+    totalGrandpas = totalGrandpas + 1;
+    grandpaCost = Math.round(grandpaCost * 1.15);
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("grandpa_cost").innerHTML = grandpaCost;
+    document.getElementById("total_grandpas").innerHTML = totalGrandpas;
+  }
+}
+
+function buyBakeries()
+{
+  if (score >= bakeryCost)
+  {
+    score = score - bakeryCost;
+    totalBakeries = totalBakeries + 1;
+    bakeryCost = Math.round(bakeryCost * 1.15);
+
+    document.getElementById("score").innerHTML = score;
+    document.getElementById("bakery_cost").innerHTML = bakeryCost;
+    document.getElementById("total_bakeries").innerHTML = totalBakeries;
+  }
+}
+
+function addToScore(amount)
+{
+  score = score + amount;
+  document.getElementById("score").innerHTML = score;
+}
+
+setInterval(function()
+{
+  score = score + totalCursors;
+  score = score + (totalGrandpas * 5) + (totalBakeries * 50)
+  document.getElementById("score").innerHTML = score;
+}, 1000);
